5cd3a1dc0d300edc45e19ec7b07112156d000f05
jehad-abdallah <gehad.abdallah30194@gmail.com>
2018-03-15 11:54:58 +0300

    Create 4.sql

---
+++ 4.sql
@@ -0,0 +1,21 @@
+select customer.Name, voucher.id_voucher
+from customer
+left join voucher on customer.id_customer = voucher.id_voucher
+where id_voucher > 10;
+
+select voucher.id_voucher, customer.id_customer
+from voucher
+right join customer on voucher.id_voucher = customer.id_customer
+where 'id_voucher'  = 10;
+
+select customer.Name, voucher.id_voucher
+from customer
+left join voucher on customer.id_customer = voucher.id_voucher
+where id_customer
+
+
+SELECT   voucher.cost, route_to_tour.id_route_to_tour, tourist_route.duration
+FROM voucher
+LEFT JOIN route_to_tour ON route_to_tour.id_route_to_tour = voucher.id_voucher
+LEFT JOIN tourist_route ON tourist_route.id_route = voucher.id_voucher
+where 
