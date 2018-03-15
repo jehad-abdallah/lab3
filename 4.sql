2bb83f9e6a4677d83f327c810c1b0e5e86187841
jehad-abdallah <gehad.abdallah30194@gmail.com>
2018-03-15 12:33:53 +0300

    Update 4.sql

--- 4.sql
+++ 4.sql
@@ -11,11 +11,12 @@ where 'id_voucher'  = 10;
 select customer.Name, voucher.id_voucher
 from customer
 left join voucher on customer.id_customer = voucher.id_voucher
-where id_customer
+where customer.id_customer= voucher.id_voucher;
 
 
 SELECT   voucher.cost, route_to_tour.id_route_to_tour, tourist_route.duration
 FROM voucher
 LEFT JOIN route_to_tour ON route_to_tour.id_route_to_tour = voucher.id_voucher
 LEFT JOIN tourist_route ON tourist_route.id_route = voucher.id_voucher
-where 
+where voucher.id_voucher = route_to_tour.id_route
+and route_to_tour.id_route_to_tour= voucher.id_voucher; 
